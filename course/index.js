1db81ce78b48349daebb96555f7ed98a32281658
mxvements <luciama2@student.42madrid.com>
2024-02-19 16:15:49 +0100

    M2-C2-L3

--- course/index.js
+++ course/index.js
@@ -1,8 +1,8 @@
 
 function showModal()
 {
-	const modal = document.getElementById("new-project-modall")
-	modal.showModal()
+	const modal = document.getElementById("new-project-modal");
+	modal.showModal();
 }
 /* this works as well:
 
@@ -13,5 +13,6 @@ const showModal = () => {
 */
 
 // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
-const newProjectBtn = document.getElementById("new-project-btn")
-newProjectBtn.addEventListener("click",showModal)
+const newProjectBtn = document.getElementById("new-project-btn");
+//console.log("new projects btn value: ", newProjectBtn); 
+newProjectBtn.addEventListener("click", showModal);
