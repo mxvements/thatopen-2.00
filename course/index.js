402fd476f2197d701cb38995126c85f418c47954
mxvements <luciama2@student.42madrid.com>
2024-02-19 16:31:28 +0100

    M2-C2-L4

--- course/index.js
+++ course/index.js
@@ -1,7 +1,9 @@
 
-function showModal()
+function showModal(id)
 {
-	const modal = document.getElementById("new-project-modal");
+	if (!id)
+		return ;
+	const modal = document.getElementById(id);
 	modal.showModal();
 }
 /* this works as well:
@@ -14,5 +16,10 @@ const showModal = () => {
 
 // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
 const newProjectBtn = document.getElementById("new-project-btn");
-//console.log("new projects btn value: ", newProjectBtn); 
-newProjectBtn.addEventListener("click", showModal);
+//console.log("new projects btn value: ", newProjectBtn);
+if (newProjectBtn) {
+	//!newProjectBtn should be true when is null (meaning: does not exist)
+	newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")});
+} else {
+	console.warn("New projects button was not found");
+}
