33ea15c9c08cb18892846a3e6955601f29846208
luciama2 <lucia.martindeaguilera@gmail.com>
2024-03-04 12:19:25 +0100

    M2-C3-L5

--- course/src/classes/Project.ts
+++ course/src/classes/Project.ts
@@ -2,6 +2,9 @@
 export type PorjectStatus = "pending" | "active" | "finished";
 export type UserRole = "architect" | "engineer" | "developer";
 
+/*	
+	Interface
+*/
 export interface IProject {
 	name: string;
 	description: string;
@@ -10,12 +13,19 @@ export interface IProject {
 	finish_date: Date;	
 }
 
+/*
+	Class
+*/ 
 export class Project implements IProject{
 	name: string;
 	description: string;
 	user_role: UserRole;
 	status: PorjectStatus;
 	finish_date: Date;
+	//class internals
+	ui: HTMLDivElement;
+	cost: number = 0;
+	progress: number = 0;
 
 	/*constructor(name, description, user_role, status, finish_date){
 		this.name = name;
@@ -31,5 +41,40 @@ export class Project implements IProject{
 		this.user_role = data.user_role;
 		this.status = data.status;
 		this.finish_date = data.finish_date;
+		this.setUI();
+	}
+
+	setUI()
+	{
+		if (this.ui) {return ;}
+		this.ui = document.createElement("div");
+		this.ui.className = "project-card"; //we need tha name for the css
+		this.ui.innerHTML = `
+			<div class="card-header">
+				<p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">HC</p>
+				<div>
+				<h5>${this.name}/h5>
+				<p>${this.description}</p>
+				</div>
+			</div>
+			<div class="card-content">
+				<div class="card-property">
+				<p style="color: #969696;">Status</p>
+				<p>${this.status}</p>
+				</div>
+				<div class="card-property">
+				<p style="color: #969696;">Role</p>
+				<p>${this.user_role}</p>
+				</div>
+				<div class="card-property">
+				<p style="color: #969696;">Cost</p>
+				<p>$${this.cost}</p>
+				</div>
+				<div class="card-property">
+				<p style="color: #969696;">Estimated Progress</p>
+				<p>${this.progress}%</p>
+				</div>
+			</div>
+		`; //copy pasted the code from the one we had on the index.html
 	}
 } 
